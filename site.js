d9fc7372ece07e462dcbf85e50a9ddeb006445fa
Lukas Korsika <lukas.korsika@gmail.com>
2012-08-31 21:21:53 +1200

    Rolled back some styling changes. Fixed hash behavior. Fixed broken floating of Reiki page.

--- site.js
+++ site.js
@@ -1,13 +1,22 @@
 $ = jQuery;
 $('.section').hide();
-$('.section.welcome').show();
-var currentSection = "welcome";
+
+var currentSection = "";
+
 $('.menu-item a').on('click', function (e) {
   e.preventDefault();
   var anchor = $(this).attr('href').substring(1);
   if (currentSection == anchor)
     return;
   currentSection = anchor;
+  window.location.hash = anchor;
   $('.section').hide(500);
   $('.section.' + anchor).show(300);
+});
+
+$(function () {
+  var hash = document.location.hash || "#welcome";
+  hash = hash.substring(1);
+  $('.section.' + hash).show();
+  var currentSection = hash;
 });
